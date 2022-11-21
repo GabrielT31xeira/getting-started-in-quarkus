621b8cadaaa88a8d5aeed5cdaf824c0425bc2e32
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2022-11-21 16:10:03 -0300

    Testes incompletos

--- src/test/java/br/com/quarkus/UserResourceTest.java
+++ src/test/java/br/com/quarkus/UserResourceTest.java
@@ -10,9 +10,33 @@ import static org.hamcrest.CoreMatchers.is;
 public class UserResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testGetAllUsersEndpoint() {
         given()
-                .when().get("/hello")
+                .when().get("/api/user")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
+    }
+    @Test
+    public void testGetOneUsersEndpoint() {
+        given()
+                .when().get("/api/user/1")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
+    }
+    @Test
+    public void testPostUsersEndpoint() {
+        given()
+                .when().post("/api/user")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
+    }
+    @Test
+    public void testDeleteUsersEndpoint() {
+        given()
+                .when().delete("/api/user/1")
                 .then()
                 .statusCode(200)
                 .body(is("Hello from RESTEasy Reactive"));
